da50e8f9d34a555d7f72a9b7c7cea78232e461de
unknown <sezeranochrisostom123@gmail.com>
2022-06-09 20:06:07 +0200

    ch(ci/cd) setup workflow
    ci/cd intergration
    
    #wpi

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import { context } from "./context";   
 
 import { schema } from "./schema/schema";
 export const server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    introspection: true,                                      
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],  
     context:context
 });
 
